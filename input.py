4f356ffe70b808d3a4fab9a1a164b4f5dfd53f2a
ESoro25 <esoroken@yahoo.com>
2021-10-09 21:09:03 -0400

    Added validation for password input

--- input.py
+++ input.py
@@ -15,41 +15,31 @@ def registration_prompt():
     return answer.lower() == 'y'
 
 def main():
-    user_info = {
-        # this password will probably be handled and hashed in a separate file
-        #"password" : password
-    }
-
-    contact_info = {
-        #"full_name" : full_name
-        #"email_address" : email_address
-    }
-
-    json_userinfo = json.dumps(user_info, indent=4)
-    json_contactinfo = json.dumps(contact_info, indent=4)
+    user_info = {}
 
     # if there are no current users registered, prompt to register
     # will need to check json file to check (not done yet)
     if not(users_registered()):
-        print("No users are registered with this client.\n")
+        print("No users are registered with this client.")
 
     if registration_prompt():
         user_info["full_name"] = input("Enter Full Name: ").strip().title()
         user_info["email_address"] = input("Enter Email Address: ")
         
         # obviously hash this password later lol
-        password = input("Enter password: ")
-        password2 = input("Re-enter password: ")
-        if not(password == password2):
-            exit()
-            user_info['password'] = password
-        else: exit()
-
-    with open("user_info.json", "w") as outfile_userinfo:
-        outfile_userinput.write(json_userinfo)
-
-    with open("contact.json", "w") as outfile_contactinfo:
-        outfile_contactinfo.write(json_contactinfo)
+        while True:
+            password = input("Enter password: ")
+            password2 = input("Re-enter password: ")
+            if not(password == password2):
+                print("Passwords do not match, try again.")
+                continue
+            else:
+                break
+        user_info['password'] = password
+    else: exit()
+    #for now, not storing this in the correct location as its easier to delete when testing
+    with open('user_info.json', 'w') as f:
+        json.dump(user_info, f, indent=4)
 
 if __name__ == '__main__':
     main()
