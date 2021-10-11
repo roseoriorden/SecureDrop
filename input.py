eae75dca79e5ff36feafee7071dff10f21c33696
ESoro25 <esoroken@yahoo.com>
2021-10-10 22:46:12 -0400

    Validation against pwd regex + error handling

--- input.py
+++ input.py
@@ -1,18 +1,24 @@
 import json
 import os.path
+import getpass
+import sys
+from password import *
+
 
 def users_registered():
     return os.path.exists("/etc/securedrop/user_info.json")
 
+
 def registration_prompt():
     while True:
-        answer = input("Do you want to register a new user (y/n)?")
+        answer = input("Do you want to register a new user (y/n)? ")
         if answer not in ('Y', 'y', 'N', 'n'):
             print("Response must be in the form: y/n")
             continue
         else:
             break
     return answer.lower() == 'y'
+
 
 def main():
     user_info = {}
@@ -25,21 +31,35 @@ def main():
     if registration_prompt():
         user_info["full_name"] = input("Enter Full Name: ").strip().title()
         user_info["email_address"] = input("Enter Email Address: ")
-        
-        # obviously hash this password later lol
+
         while True:
-            password = input("Enter password: ")
-            password2 = input("Re-enter password: ")
-            if not(password == password2):
-                print("Passwords do not match, try again.")
-                continue
-            else:
-                break
-        user_info['password'] = password
-    else: exit()
-    #for now, not storing this in the correct location as its easier to delete when testing
-    with open('user_info.json', 'w') as f:
-        json.dump(user_info, f, indent=4)
+            try:
+                password = getpass.getpass(prompt="Enter password: ")
+                password2 = getpass.getpass(prompt="Re-enter password: ")
+
+                if not(password == password2):
+                    print("Passwords do not match, try again.")
+                    continue
+
+                if meets_requirements(password):
+                    user_info['password'] = salt_and_hash(password)
+                    print("Passwords Match.")
+                    break
+                else:
+                    print("Password does not meet strength requirements!")
+                    continue
+            except KeyboardInterrupt:
+                sys.exit()
+    else:
+        sys.exit()
+    # for now, not storing this in the correct location as its easier to
+    try:
+        with open('user_info.json', 'w') as f:
+            json.dump(user_info, f, indent=4)
+        print("User Registered.")
+    except Exception:
+        sys.exit("Unable to write to file 'user_info.json'")
+
 
 if __name__ == '__main__':
     main()
