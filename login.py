698e7d780caea4cf1cee03f715750a2bd0db1828
ESoro25 <esoroken@yahoo.com>
2021-10-19 19:23:44 -0400

    Added login functionality

---
+++ login.py
@@ -0,0 +1,38 @@
+import json
+import getpass
+import sys
+from base64 import b64encode
+from Crypto.Hash import SHA256
+from Crypto.Protocol.KDF import bcrypt_check
+
+
+def main():
+
+    with open('user_info.json', 'r') as f:
+        user_info = json.loads(f.read())
+
+    while True:
+        try:
+            email = input("Enter Email Address: ")
+            password = getpass.getpass(prompt="Enter Password: ")
+
+            if(user_info['email_address'] == email):
+                try:
+                    pwd_b64 = b64encode(SHA256.new(password.encode()).digest())
+                    bcrypt_check(pwd_b64.decode(), user_info['password'])
+
+                    print("Welcome to Secure Drop.")
+                    print("Type \"help\" For Commands.")
+                    break
+                except ValueError:
+                    print("Email and Password Combination Invalid.")
+                    continue
+            else:
+                print("Email and Password Combination Invalid.")
+                continue
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+if __name__ == '__main__':
+    main()
--- password.py
+++ password.py
@@ -6,13 +6,13 @@ from Crypto.Random import get_random_bytes
 
 
 def meets_requirements(pwd):
-    # 8 chars, min 1 uppercase, min 1 lowercase, min 1 number or letter
+    # 8 chars, min 1 uppercase, min 1 lowercase, min 1 number or symbol
     pattern = r"^(?=.*[a-z])(?=.*[A-Z])((?=.*[@$!%*?&])|(?=.*\d))[A-Za-z\d@$!%*?&]{8,}$"
     return re.search(pattern, pwd)
 
 
 def salt_and_hash(pwd):
     salt = get_random_bytes(16)
-    pwd_as_bytes = pwd.encode()
-    pwd_b64 = b64encode(SHA256.new(pwd_as_bytes).digest())
-    return bcrypt(pwd, 12, salt).decode()
+    pwd_b64 = b64encode(SHA256.new(pwd.encode()).digest())
+    return bcrypt(pwd_b64, 12, salt).decode()
+
