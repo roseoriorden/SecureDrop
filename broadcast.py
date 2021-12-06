4575b6979c2fb7e6974d31cdd515f67c38ff4e5f
ESoro25 <esoroken@yahoo.com>
2021-12-06 17:43:19 -0500

    tcp connection and contact hash validation

--- broadcast.py
+++ broadcast.py
@@ -2,8 +2,16 @@ import socket
 import time
 import threading
 import json
+from base64 import b64encode, b64decode
 from Crypto.Hash import SHA256
 from contacts import decrypt_contacts
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
 def init_broadcast_socket():
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -16,6 +24,19 @@ def init_client_socket():
     client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     return client
+
+def init_tcp_server_socket():
+    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcp_server.bind((get_ip(),5000))
+    tcp_server.listen(5)
+    return tcp_server
+
+def init_tcp_client_socket(IP):
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcp_client.connect((IP, 5000))
+    return tcp_client
 
 
 def broadcaster(socket):
@@ -31,6 +52,32 @@ def receiver(socket):
         check_incoming_hash(data)
 #        print("received " + data.decode() + " from " + addr[0])
 
+def serve_tcp(socket):
+    client, addr = socket.accept()
+    while True:
+        payload = client.recv(1024)
+        #print(payload.decode())
+        if b'securedrop' in b64decode(payload):
+            #print(b64decode(payload))
+            decoded_hash = b64decode(payload).decode().replace('securedrop','')
+            print(decoded_hash)
+
+            client.send(b'1') if check_incoming_hash(decoded_hash) else client.send(b'0')
+        else:
+            pass
+    socket.close()
+
+def send_tcp(socket):
+    payload = b64encode(b'securedrop'+get_own_hash().encode())
+    while True:
+        print("sending payload " + payload.decode())
+
+        socket.sendall(payload)
+        data = socket.recv(1024)
+        print(data.decode())
+        time.sleep(1)
+    socket.close()
+
 def get_own_hash():
         with open('user_info.json', 'r') as f:
             user_info = json.load(f)
@@ -40,14 +87,18 @@ def check_incoming_hash(data):
     contacts = decrypt_contacts()
     
     for contact in contacts:
-        print(contact['email_hash'])
-        if contact['email_hash'] == data.decode():
-            print("Your contact " + contact['full_name'] + " is online!")
+        #print(contact['email_hash'])
+        return contact['email_hash'] == data
+            #print("Your contact " + contact['full_name'] + " is online!")
 
 def main():
-
+    
     t1 = threading.Thread(target=broadcaster,args=(init_broadcast_socket(),))
     t2 = threading.Thread(target=receiver,args=(init_client_socket(),))
+
+    t1 = threading.Thread(target=serve_tcp, args=(init_tcp_server_socket(),))
+    t2 = threading.Thread(target=send_tcp, args=(init_tcp_client_socket(get_ip()),)) #BROADCASTERS IP?
+
 
     t1.start()
     t2.start()
