24d72c2a3840d9f4cb59253b50d204f6355b87b5
ESoro25 <esoroken@yahoo.com>
2021-12-12 22:18:57 -0500

    Removed debug prints

--- contacts.py
+++ contacts.py
@@ -6,12 +6,9 @@ from Crypto.Hash import SHA256
 from blake3 import blake3
 import os.path
 
-
-from input import main
-
 def decrypt_contacts():
     try:
-        with open("user_contacts_enc.json", "rb") as f:
+        with open("user_contacts.json", "rb") as f:
             nonce, tag, ciphertext = [ f.read(x) for x in (16, 16, -1) ]
 
         cipher = AES.new(get_derived_key(), AES.MODE_EAX, nonce)
@@ -20,12 +17,28 @@ def decrypt_contacts():
         return json.loads(data.decode())
 
     except FileNotFoundError:
-        sys.exit("No contacts found!")
+        with open('user_contacts.json', 'w') as f:
+            contacts_list.append(new_contact)
+            json.dump(contacts_list, f, indent=4)
 
+        with open("user_contacts.json", "rb") as f:
+            plaintext = f.read()
+        
+        cipher = AES.new(get_derived_key(), AES.MODE_EAX)
+        ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+
+        with open("user_contacts.json", "wb") as f:
+            [ f.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+
+        print('Contact Added.')
+        
 def list():
     print(json.dumps(decrypt_contacts(),indent=4))
 
 def main():
+    global new_contact
+    global contacts_list
+
     new_contact = {}
     contacts_list = []
 
@@ -34,46 +47,31 @@ def main():
         new_contact['email_address'] = input("Enter Email Address of contact: ")
         new_contact['email_hash'] = SHA256.new(new_contact['email_address'].encode()).hexdigest()
        
-        try:
-            with open('user_contacts.json', 'r+') as f:
-                contacts = json.load(f)
+        contacts = decrypt_contacts()
 
-                for contact in contacts:
-                    if contact['email_address'] == new_contact['email_address']:
-                        contact.update(new_contact)
-                        f.seek(0)
-                        json.dump(contacts, f, indent=4)
-                        break
+        for contact in contacts:
+            if contact['email_address'] == new_contact['email_address']:
+                contact.update(new_contact)
+                f.seek(0)
+                json.dump(contacts, f, indent=4)
+                break
 
-                else:
-                    contacts.append(new_contact)
-                    f.seek(0)
-                    json.dump(contacts, f, indent=4)
+        else:
+            contacts.append(new_contact)
+            f.seek(0)
+            json.dump(contacts, f, indent=4)
 
-        except FileNotFoundError:
-            with open('user_contacts.json', 'w') as f:
-                contacts_list.append(new_contact)
-                json.dump(contacts_list, f, indent=4)
         print("Contact Added.")
 
     except KeyboardInterrupt:
         sys.exit()
-
-    with open("user_contacts.json", "rb") as f:
-        plaintext = f.read()
-    
-    cipher = AES.new(get_derived_key(), AES.MODE_EAX)
-    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
-
-    with open("user_contacts_enc.json", "wb") as f:
-        [ f.write(x) for x in (cipher.nonce, tag, ciphertext) ]
 
 def get_derived_key():
     try:
         with open('user_info.json', 'r') as f:
             user_info = json.load(f)
         
-        key_material = user_info['email_address']
+        key_material = user_info['password']
 
         with open('/etc/machine-id','r') as f:
             context = f.read()
@@ -82,7 +80,6 @@ def get_derived_key():
 
     except FileNotFoundError as missing_file:
         sys.exit("Could not open file " + missing_file.filename)
-    
         
 if __name__ == "__main__":
     main()
--- send_file.py
+++ send_file.py
@@ -1,15 +1,12 @@
 import socket
-import tqdm # progress bars (not needed but fun)
 import os
-from os import path
 import json
 import sys
-from contacts import decrypt_contacts
 import threading
-from base64 import b64encode
-from base64 import b64decode
 import broadcast
 import time
+from contacts import decrypt_contacts
+from base64 import b64encode, b64decode
 
 def init(email, filepath):
     # first check that the contact exists
@@ -24,13 +21,13 @@ def init(email, filepath):
         print("Contact is not registered.\nExiting file transfer.")
         sys.exit()
     else:
-        print('contact is registered.')
+        print('Contact is registered.')
     # then check that filepath exists
     if not os.path.exists(filepath):
         print(filepath + ": No such file or directory.\nExiting file transfer.")
         sys.exit()
     else:
-        print('filepath is registered')
+        print('Filepath is registered')
     # then check that contact is online
     
     # get IP from dictionary
@@ -38,18 +35,15 @@ def init(email, filepath):
     contacts_dict = broadcast.return_contacts_dict()
     if email in contacts_dict:
         contact_is_online = True
-        print('contact is online, searching for IP...')
     if not contact_is_online:
-        print('contact is not online')
+        print('Contact is not online')
         try:
             sys.exit()
         except:
-            print('could not exit')
+            print('Could not exit')
     else:
         ip_addr = contacts_dict[email]
-        print('contact is online')
-    print(ip_addr)
-    # init_tcp_client_socket(IP)
+        print('Contact is online')
     accept_transfer = send_request(init_tcp_client_socket(ip_addr))
     # ask receiver if they would like to receive file
 
@@ -60,24 +54,17 @@ def init(email, filepath):
         print("Recipient declined.\nExiting file transfer.")
         sys.exit()
 
-
-def receive_file():
-    # 
-    print("Receiving file")
-
 def init_tcp_server_socket():
     tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp_server.bind((broadcast.get_ip(),5010)) #Bind to localhost for testing, replace with get_ip() in production
     tcp_server.listen(5)
-    print('Started TCP Server on ', broadcast.get_ip())
     return tcp_server
 
 def init_tcp_client_socket(IP):
     tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     tcp_client.connect((IP, 5010))
-    print('Started TCP Client...')
     return tcp_client
 
 def serve_tcp(socket):
@@ -86,12 +73,8 @@ def serve_tcp(socket):
         threading.Thread(target=validate_payload, args=(client,addr),daemon=True).start()
 
 def validate_payload(client, addr):
-    #while True: #Does this need to be a loop?
-        # contacts_dict = broadcast.return_contacts_dict()
-        # contact_email = contacts_dict[client]
         payload = client.recv(4096)
         accept = ''
-        # print(b64decode(payload))
         try:
             if b'requestsd' in b64decode(payload):
                 decoded_hash = b64decode(payload).decode().replace('requestsd','')
@@ -105,11 +88,7 @@ def validate_payload(client, addr):
             data += recvall(client).decode()
             with open('output', 'w') as outfile:
                 outfile.write(data)
-        # except:
-        #     data = payload
-        #     data.append(recvall(client))
-        client.close() #Should we close the connection after?
-
+        client.close()
 
 def recvall(sock):
     BUFF_SIZE = 4096
@@ -122,47 +101,28 @@ def recvall(sock):
     return data
 
 def send_tcp(socket, filepath):
-    # payload = b64encode(b'securedrop'+get_own_hash().encode())
-    #while True:
-    # print("Sending TCP Payload: " + payload.decode())
     filesize = os.path.getsize(filepath)
-    # progress = tqdm.tqdm(range(filesize), f"Sending {filepath}", 
-    #             unit="B", unit_scale=True, unit_divisor=1024)
     with open(filepath, 'rb') as f:
         data = f.read()
     socket.sendall(data)
-    # progress.update(len(bytes_read))
-    # socket.shutdown(socket.SHUT_WR)
     socket.close()
-    print("transfer complete")
-    # data = socket.recv(1024)
-    # print("Recieved TCP Payload: " + data.decode())
+    print("Transfer complete")
 
 def send_request(socket):
-    # own_hash = broadcast.get_own_hash()
-    # payload = b64encode(b'requestsd'+own_hash)
     payload = b64encode(b'requestsd'+broadcast.get_own_hash().encode())
-    #while True:
-    #print("Sending TCP Payload: " + payload.decode())
     socket.sendall(payload)
     data = socket.recv(1024)
-    #print('TCP DATA ', data.decode())
 
     if data.decode() == '1':
         return True
     else:
         return False
-        #socket.close()
 
 def init_file_tcp_server():
     threading.Thread(target=serve_tcp, args=(init_tcp_server_socket(),),daemon=True).start() #TCP Server
 
 def main(email, filepath):
-    #if email == '':
-        #email = "rose"
-        #filepath = "rose"
-    
     init(email, filepath)
 
 if __name__ == "__main__":
-    main('evan1', 'longtest')
+    main('evan1', 'longtest') #debug
